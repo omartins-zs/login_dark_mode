d2d369f39f3a31ac708929a6f9bbec148b9e9725
omartins-zs <gabrielmatheus209@gmail.com>
2023-10-09 11:09:39 -0300

    Criando Function SwitchTheme(Troca Tema)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 let body = document.body;
-let loginBox = document.querySelector("login-box");
+let loginBox = document.querySelector(".login-box");
 let h1 = document.getElementsByClassName("h1")[0];
 let inputs = document.querySelectorAll("input");
 let loginButton = document.getElementById("login-button");
+
+function switchTheme() {
+    loginBox.classList.toggle("dark-mode");
+    body.classList.toggle("dark-mode");
+    h1.classList.toggle("dark-mode");
+
+    inputs.forEach(input => {
+        input.classList.toggle("dark-mode");
+    });
+
+    loginButton.classList.toggle("dark-mode");
+    h2.classList.toggle("dark-mode");
+}
