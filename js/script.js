3ef610b8bc1a1ae0480cdc244272401fbe0a492b
omartins-zs <gabrielmatheus209@gmail.com>
2023-10-09 11:06:14 -0300

    Declarando as Variaveis

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-let = "";
+let body = document.body;
+let loginBox = document.querySelector("login-box");
+let h1 = document.getElementsByClassName("h1")[0];
+let inputs = document.querySelectorAll("input");
+let loginButton = document.getElementById("login-button");
